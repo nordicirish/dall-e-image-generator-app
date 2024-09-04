2f7941e929cf869ad2b866ae7e65bc639681c414
Roger Graham <rogergraham147@gmail.com>
2024-09-04 22:01:03 +0300

    Change duration to tailwindclass duration-700 to avoid warning during compilation

--- app/image-generation/page.tsx
+++ app/image-generation/page.tsx
@@ -188,7 +188,7 @@ export default function ImageGeneration() {
                 <Progress.Indicator
                   className={`w-full h-full ${
                     imageUrl ? "bg-green-500" : "bg-amber-500"
-                  } transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]`}
+                  } transition-transform duration-700 ease-[cubic-bezier(0.65,0,0.35,1)]`}
                   style={{ transform: `translateX(-${100 - progress}%)` }}
                 />
               </Progress.Root>
